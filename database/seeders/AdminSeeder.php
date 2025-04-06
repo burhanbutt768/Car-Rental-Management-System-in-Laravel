7cb45bcc4f07a55c5a9c1390234925da4933f035
Burhan Abid Butt <138368461+burhanbutt768@users.noreply.github.com>
2025-04-06 13:48:22 +0500

    Add files via upload

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('admins')->insert([
+            [
+                'name' => 'Admin One',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Admin Two',
+                'email' => 'admin2@example.com',
+                'password' => Hash::make('password456'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
